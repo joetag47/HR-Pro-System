75da54c23e09fe059d22d060d4f07fff87d29ac0
bkantwi <bernardantwi2010@gmail.com>
2022-10-29 16:21:23 +0000

    sidebar update

--- resources/views/layouts/sidebar_menu.blade.php
+++ resources/views/layouts/sidebar_menu.blade.php
@@ -106,7 +106,6 @@
                         </li>
                         <!--end::Item-->
                         <!--begin::Item-->
-                        @role('admin')
                         <<li class="nav-item mb-3" data-toggle="tooltip" data-placement="right" data-container="body" data-boundary="window" title="Company Profile">
                             <a href="{{ route('userManagement.index') }}" class="nav-link btn btn-icon btn-clean btn-lg">
 										<span class="svg-icon svg-icon-primary svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-05-14-112058/theme/html/demo3/dist/../src/media/svg/icons/Home/Commode1.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
@@ -119,7 +118,6 @@
                                         </span>
                             </a>
                         </li>
-                        @endrole
                         <!--end::Item-->
                         <!--begin::Item-->
                         <li class="nav-item mb-3" data-toggle="tooltip" data-placement="right" data-container="body" data-boundary="window" title="Finance &amp; Accounting">
