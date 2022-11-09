ce901606f21730f4ef85e0df18617fee10329503
bkantwi <bernardantwi2010@gmail.com>
2022-11-09 21:10:41 +0000

    multi select for permissions enabled

--- resources/views/rolesandpermissions/create.blade.php
+++ resources/views/rolesandpermissions/create.blade.php
@@ -26,12 +26,12 @@
                     </div>
                     <!--end::Info-->
                     <!--begin::Toolbar-->
-                    <div class="d-flex align-items-center flex-wrap">
-                        <!--begin::Button-->
-                        <a href="#" class="btn btn-primary btn-shadow" data-toggle="modal" data-target="#departmentAddModal">
-                            <i class="flaticon-plus"></i> Add Role
-                        </a>
-                    </div>
+{{--                    <div class="d-flex align-items-center flex-wrap">--}}
+{{--                        <!--begin::Button-->--}}
+{{--                        <a href="#" class="btn btn-primary btn-shadow" data-toggle="modal" data-target="#departmentAddModal">--}}
+{{--                            <i class="flaticon-plus"></i> Add Role--}}
+{{--                        </a>--}}
+{{--                    </div>--}}
                 </div>
             </div>
             <div class="container">
@@ -58,24 +58,32 @@
                                         <label class="form-check-label" for="checkPermissionAll">All</label>
                                     </div>
                                     <hr>
-
+                                    @php $i = 1; @endphp
                                     @foreach ($permission_groups as $group)
                                         <div class="row">
                                             <div class="col-3">
                                                 <div class="form-check">
-                                                    <input type="checkbox" class="form-check-input" name="permissions[]" id="checkPermission" value="{{ $group->name }}">
+{{--                                                    name="permissions[]"--}}
+                                                    <input type="checkbox" class="form-check-input" id="{{ $i }}Management" value="{{ $group->name }}" onclick="checkPermissionByGroup('role-{{ $i }}-management-checkbox', this)">
                                                     <label class="form-check-label" for="checkPermission">{{ $group->name }}</label>
                                                 </div>
                                             </div>
-                                            <div class="clo-9">
+                                            <div class="clo-9 role-{{ $i }}-management-checkbox">
+                                                @php
+//                                                    $permissions = App\User::getpermissionsByGroupName($group->name);
+                                                    $j = 1;
+                                                @endphp
                                                 @foreach ($permissions as $permission)
                                                     <div class="form-check">
                                                         <input type="checkbox" class="form-check-input" name="permissions[]" id="checkPermission{{ $permission->id }}" value="{{ $permission->name }}">
                                                         <label class="form-check-label" for="checkPermission{{ $permission->id }}">{{ $permission->name }}</label>
                                                     </div>
+                                                    @php $j++; @endphp
                                                 @endforeach
+                                                <br >
                                             </div>
                                         </div>
+                                        @php $i++; @endphp
                                     @endforeach
                                 </div>
                                 <button type="submit" class="btn btn-primary mt-4 pr-1 pl-4">Save Role</button>
@@ -118,5 +126,16 @@
                         $('input[type=checkbox]').prop('checked', false);
                     }
                 })
+
+                function checkPermissionByGroup(className, checkThis){
+                    const groupIdName = $("#"+checkThis.id);
+                    const classCheckBox = $('.'+className+' input');
+
+                    if(groupIdName.is(':checked')){
+                        classCheckBox.prop('checked', true);
+                    }else{
+                        classCheckBox.prop('checked', false);
+                    }
+                }
             </script>
     @endpush
